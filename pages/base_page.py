1a2b6add7ae4b49a6ece677da28037d7d04d5d62
ShilovVL <v.shilov.tomsk@gmail.com>
2021-03-09 12:34:38 +0700

    добавлен переход на login страницу

--- pages/base_page.py
+++ pages/base_page.py
@@ -16,6 +16,3 @@ class BasePage():
         except NoSuchElementException:
             return False
         return True
-
-    # def should_be_login_link(self):
-    #     assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
