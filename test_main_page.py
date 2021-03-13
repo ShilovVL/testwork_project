d85e834613de6333a4795ce5a734ee57c681897f
ShilovVL <v.shilov.tomsk@gmail.com>
2021-03-13 21:50:53 +0700

    small edit for pep8

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,8 @@
-
 from .pages.main_page import MainPage # Windows нужно без точки, 4 Linux - .pages.main_page
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
 import pytest
+
 
 def test_guest_can_see_login_url(browser):
     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
@@ -10,17 +10,20 @@ def test_guest_can_see_login_url(browser):
     page.open()
     page.should_be_login_url()
 
+
 def test_guest_can_see_login_form(browser):
     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
     page = LoginPage(browser, link)
     page.open()
     page.should_be_login_form()
 
+
 def test_guest_can_see_register_form(browser):
     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
     page = LoginPage(browser, link)
     page.open()
     page.should_be_register_form()
+
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -38,6 +41,7 @@ class TestLoginMainPage():
         page = MainPage(browser, link)
         page.open()
         page.go_to_login_page()
+
 
     def test_guest_should_see_login_link(self, browser):
         link = "http://selenium1py.pythonanywhere.com/"
